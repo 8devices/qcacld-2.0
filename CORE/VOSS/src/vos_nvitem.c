8af498f613555fba145e62e45f86b7a53636f837
Žilvinas Valinskas <zilvinas@8devices.com>
2021-01-26 10:31:01 +0200

    XXX: just to make it compile.

--- CORE/VOSS/src/vos_nvitem.c
+++ CORE/VOSS/src/vos_nvitem.c
@@ -1543,10 +1543,12 @@ VOS_STATUS vos_nv_getRegDomainFromCountryCode( v_REGDOMAIN_t *pRegDomain,
             vos_set_cc_source(CNSS_SOURCE_11D);
 
         INIT_COMPLETION(pHddCtx->reg_init);
+#if 0
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,9,0)) || defined(WITH_BACKPORTS)
         regulatory_hint_user(country_code, NL80211_USER_REG_HINT_USER);
 #else
         regulatory_hint_user(country_code);
+#endif
 #endif
         wait_result = wait_for_completion_interruptible_timeout(
                                &pHddCtx->reg_init,
